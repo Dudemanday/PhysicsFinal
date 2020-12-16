8d200307d0a3c03be7bfafa8040de015df1494f7
NickW <nickmwootton@gmail.com>
2020-12-16 14:52:17 -0500

    First push

---
+++ GAME2005-F2020-FinalTest/Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+
+    public void LoadMain()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
